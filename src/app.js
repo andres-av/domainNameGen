04a79b43dc43d641636d1ad2690d280df393bb02
andres-av <jandresav1793@gmail.com>
2022-07-06 19:28:02 +0000

    forEach implementation

--- src/app.js
+++ src/app.js
@@ -14,15 +14,25 @@ window.onload = function() {
 
   let domain = [];
 
-  for (var i = 0; i < pronoun.length; i++) {
-    for (var j = 0; j < adj.length; j++) {
-      for (let x = 0; x < noun.length; x++) {
-        for (let e = 0; e < ext.length; e++) {
-          domain.push(pronoun[i] + adj[j] + noun[x] + ext[e]);
-        }
-      }
-    }
-  }
+  // for (var i = 0; i < pronoun.length; i++) {
+  //   for (var j = 0; j < adj.length; j++) {
+  //     for (let x = 0; x < noun.length; x++) {
+  //       for (let e = 0; e < ext.length; e++) {
+  //         domain.push(pronoun[i] + adj[j] + noun[x] + ext[e]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  pronoun.forEach(pronoun => {
+    adj.forEach(adj => {
+      noun.forEach(noun => {
+        ext.forEach(ext => {
+          domain.push(pronoun + adj + noun + ext);
+        });
+      });
+    });
+  });
 
   let domainList = document.querySelector(".domainsList");
 
